1413e0b1614d808dc29accda603dbc415a59bbd2
mxxikr <mxxikr@gmail.com>
2022-10-05 00:26:38 +0900

    x만큼 간격이 있는 n개의 숫자 풀이 2

--- python/x_interval_n.py
+++ python/x_interval_n.py
@@ -12,9 +12,6 @@ x	n	answer
 """
 
 def solution(x, n):
-    answer = []
-    for i in range(1, n+1):
-        answer.append(x*i)
-    return answer
+    return [x * i for i in range(1, n+1)]
 
 solution(2, 5)
