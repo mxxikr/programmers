153c62c78b34f502e3840112346645e68fa75e24
mxxikr <mxxikr@gmail.com>
2022-10-12 18:26:52 +0900

    수박수박수박수박수박수? 풀이 2

--- python/watermelon.py
+++ python/watermelon.py
@@ -6,10 +6,4 @@ n은 길이 10,000이하인 자연수입니다.
 """
 
 def solution(n):
-    answer = ""
-    for i in range(1, n+1):
-        if i % 2 != 0:
-            answer += "수"
-        else:
-            answer += "박"
-    return answer
+    return ("수박" * n)[:n]
